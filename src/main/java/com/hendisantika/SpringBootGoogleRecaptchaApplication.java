5e661542b9de2434415ce6b6d1844c310b0fc632
Hendi Santika <hendisantika@gmail.com>
2021-10-04 05:53:10 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootGoogleRecaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleRecaptchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleRecaptchaApplication.class, args);
+    }
+
+}
