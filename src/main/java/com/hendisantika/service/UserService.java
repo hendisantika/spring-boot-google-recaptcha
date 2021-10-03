e130a0f6a573fcaf7e6b3c2f875b457c06657c8d
Hendi Santika <hendisantika@gmail.com>
2021-10-04 05:58:13 +0700

    #7 - Add createUser in UserService

--- src/main/java/com/hendisantika/service/UserService.java
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -1,5 +1,6 @@
 package com.hendisantika.service;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +18,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public Integer createUser(User user) {
+        Integer userId = userRepository.save(user).getId();
+        return userId;
+    }
 }
