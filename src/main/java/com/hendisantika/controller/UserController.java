7e76ba261a69ffaa8758f781bf9fb2298abb1023
Hendi Santika <hendisantika@gmail.com>
2021-10-04 06:02:39 +0700

    #13 - Add registerUser in UserController

--- src/main/java/com/hendisantika/controller/UserController.java
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.service.UserService;
 import com.hendisantika.validator.CaptchaValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,4 +26,10 @@ public class UserController {
 
     @Autowired
     private CaptchaValidator validator;
+
+    @GetMapping("/register")
+    public String registerUser(Model model) {
+        model.addAttribute("user", new User());
+        return "registerUser";
+    }
 }
