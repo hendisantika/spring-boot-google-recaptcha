3befb59dcd6c3d645fdc68b34903f028e8f623ed
Hendi Santika <hendisantika@gmail.com>
2021-10-04 05:59:27 +0700

    #10 - Add restTemplate Bean in Main Class

--- src/main/java/com/hendisantika/SpringBootGoogleRecaptchaApplication.java
+++ src/main/java/com/hendisantika/SpringBootGoogleRecaptchaApplication.java
@@ -2,6 +2,8 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootGoogleRecaptchaApplication {
@@ -10,4 +12,8 @@ public class SpringBootGoogleRecaptchaApplication {
         SpringApplication.run(SpringBootGoogleRecaptchaApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
